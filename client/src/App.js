5d065b9bf013a550f6e8b226cdbf366b78145275
jpwebdev <jjpwebdev@gmail.com>
2018-02-16 16:08:43 -0500

    displaying user's speech on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
+
 class App extends Component {
+  state = {}
+  
+  onListenClick = () => {
+
+    fetch('http://localhost:3002/api/speech-to-text/token')
+      .then(function(response) {
+          return response.text();
+      }).then((token) => {
+        console.log('token is', token)
+        var stream = recognizeMic({
+            token: token,
+            objectMode: true, // send objects instead of text
+            extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
+            format: false // optional - performs basic formatting on the results such as capitals an periods
+        });
+        stream.on('data', (data) => {
+          this.setState({
+            text: data.alternatives[0].transcript
+          })
+        });
+        stream.on('error', function(err) {
+            console.log(err);
+        });
+        document.querySelector('#stop').onclick = stream.stop.bind(stream);
+      }).catch(function(error) {
+          console.log(error);
+      });
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.onListenClick}>Listen to microphone</button>
+        <div style={{fontSize: '40px'}}>{this.state.text}</div>
+
       </div>
     );
   }
