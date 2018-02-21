97fce8b52b6d97ea23162b2957b5e9e908276ad1
jpwebdev <jjpwebdev@gmail.com>
2018-02-20 22:42:56 -0500

    refactoring App and Search

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,60 @@ import './App.css';
 import Search from './Components/Search';
 import SearchResult from './Components/SearchResults';
 
-class App extends Component {
+// Utils
+import API from './utils/API';
 
+class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  // Activate SpeechRecognition on page load to always be listening
+  componentDidMount() {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new window.SpeechRecognition();
+    recognition.interimResults = false;
+
+    recognition.onresult = event => {
+      const transcript = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+      console.log(transcript);
+      // Check for keywords
+      const ingredients = ['chicken recipes', 'meat recipes', 'pasta recipes', 'fish recipes', 'rice recipes'];
+
+      // Check for matches to run the specific query
+      ingredients.forEach(value => {
+        if (transcript.includes(value)) {
+          API.searchRecipes(value)
+            .then(edamamRes => {
+              let recipesCopy = this.state.recipes.slice();
+              edamamRes.data.forEach(value => recipesCopy.push(value))
+              
+              this.setState({ recipes: recipesCopy })
+              console.log(edamamRes.data)
+            })
+            .catch(err => console.log(err))
+        }
+      })
+    }
+
+    // Keep the Speech API open
+    // recognition.onspeechend = () => recognition.start();
+    recognition.start();
+  }
+  
+  // Function to handle manual search
+  handleManualSearch = searchResult => this.setState({ recipes: searchResult });
 
   render() {
     
     return (
       <div className="container">
         <h1>Cheff W</h1>
-        <Search />
+        <Search manualSearch={this.handleManualSearch} />
         <ul>
           <SearchResult />
         </ul>
--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -5,46 +5,15 @@ import React, { Component } from 'react';
 import API from '../utils/API';
 
 class Search extends Component {
-
-  componentDidMount() {
-    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-    const recognition = new window.SpeechRecognition();
-    recognition.interimResults = true;
-
-    recognition.onresult = event => {
-      const transcript = Array.from(event.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('')
-      console.log(transcript);
-      // Check for keywords
-      const ingredients = ['chicken recipes', 'meat recipes', 'pasta recipes', 'fish recipes', 'rice recipes'];
-      // const commands = []
-
-      ingredients.forEach(value => {
-        if (transcript.includes(value)) {
-          console.log(value)
-          API.searchRecipes(value)
-            .then(edamamRes => console.log(edamamRes.data))
-            .catch(err => console.log(err))
-        }
-      })
-    }
-
-    // Keep the Speech API open
-    // recognition.onspeechend = () => recognition.start();
-    recognition.start();
-}
-
-  handleChange = event => {
-    this.setState({ query: event.target.value });
-  }
+  state = { query: '' }
 
   handleSearch = event => {
     event.preventDefault();
     API.searchRecipes(this.state.query)
-      .then(edamamRes => console.log(edamamRes.data))
+      .then(edamamRes => {
+        // return the result to the parent component
+        this.props.manualSearch(edamamRes.data)
+      })
       .catch(err => console.log(err))
   }
 
@@ -52,16 +21,20 @@ class Search extends Component {
 
     return (
 
-        <div className="form-group">
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Ex. Say 'Chicken recipes'"
-            />
-            <button onClick={this.speech} type="submit" className="btn btn-primary mt-3">Listen</button>
-            <p>I heard</p>
-
-        </div>
+      <div className="form-group">
+        <input 
+          onChange={event => this.setState({ query: event.target.value })}
+          type="text" 
+          className="form-control" 
+          placeholder="Ex. Say 'Chicken recipes'"
+        />
+        <button 
+          onClick={this.handleSearch}
+          type="submit" 
+          className="btn btn-primary mt-3">
+          Listen
+        </button>
+      </div>
     );
   }
 }
